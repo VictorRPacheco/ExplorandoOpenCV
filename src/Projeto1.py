196c0e530ee70e1cb858bc71a374eeea12f578aa
VictorRPacheco <victorrpacheco98@gmail.com>
2019-04-02 23:07:10 -0300

    Codigo feito

--- src/Projeto1.py
+++ src/Projeto1.py
@@ -4,11 +4,11 @@
 -> Abre uma imagem
 -> Quando clicado na imagem ideintifica onde foi o clique
 -> Idenfica a cor de onde foi clicado
--> Calcula as cores similares a ela e detaca-as de vermelho
-Rode o codigo com -> python Trabalho.py
-Para rodar o codigo eh necessaria uma pasta ../data com um arquivo test.jpg
-
-Para sair pressione 'q'
+-> Cria matriz com True no local dos pixels a serem destacados
+-> Destaca em vermelhos os pixels
+Rode o codigo com -> python Projeto1.py
+Para rodar os requisitos 1, 2 e 3 eh necessaria uma pasta ../data 
+com arquivos de teste ou usar seus proṕrios arquivos
 """
 
 import cv2
@@ -16,6 +16,7 @@ import numpy as np
 color = None
 
 
+# Funcao 
 def Cor_e_Posicao(event, x, y, flags, param):
 	global clickPoint
 	global color
@@ -31,6 +32,7 @@ def Cor_e_Posicao(event, x, y, flags, param):
 			print "Posicao [X, Y]: ", (x, y), "Cor: ", str(color)
 
 
+# Selecao de qual requisito executar
 print "╔═══════════════════════════════════════╗"
 print "║	Escolha um dos requisitos	║"
 print "╠═══════════════════════╦═══════════════╣"
@@ -58,44 +60,110 @@ if flag == 1 or flag == 2:
 	elif file == '2':
 		image = cv2.imread("../data/testPeB.jpg")
 	else:
-		try:
-			image = cv2.imread(file)
-		except:
-			print "Arquivo inválido, abortando"
-			cv2.destroyAllWindows()
+		image = cv2.imread(file)
+	if image.empty():
+		print "Arquivo inválido, abortando"
+		cv2.destroyAllWindows()
 	raw = image.copy()
 	cv2.namedWindow("Trabalho")
 	cv2.setMouseCallback("Trabalho", Cor_e_Posicao)
-
 
 if flag == 1:
 	print "╔═══════════════════════════════╗"
 	print "║	Para sair pressione 'q'	║"
 	print "╚═══════════════════════════════╝\n"
-	cv2.imshow("Trabalho", raw)
+	while True:	
+		cv2.imshow("Trabalho", raw)
+		if cv2.waitKey(25) & 0xFF == ord('q'):
+			cv2.destroyAllWindows()
+			break
 
-if flag == 2:
+elif flag == 2:
 	while True:
 		cv2.imshow("Trabalho", raw)
 		if color is not None:
-		        # Separa as 3 cores
-		        R = raw[:, :, 0].astype(np.float32)
-		        G = raw[:, :, 1].astype(np.float32)
-		        B = raw[:, :, 2].astype(np.float32)
-		        raw = image.copy()
-		        # Calcula quais pixels serão marcados com base na distância euclidiana para imagens coloridas
+			# Separa as 3 cores
+			R = raw[:, :, 0].astype(np.float32)
+			G = raw[:, :, 1].astype(np.float32)
+			B = raw[:, :, 2].astype(np.float32)
+			raw = image.copy()
+			# Calcula quais pixels serão marcados com base na distância euclidiana para imagens coloridas
 			if color[0] == color[1] == color[2]:
-			        euclidiano = ((R - color[0]) ** 2) + ((G - color[1]) ** 2) + ((B - color[2]) ** 2)
-			        mask = euclidiano < (13 ** 2)
+				euclidiano = ((R - color[0]) ** 2) + ((G - color[1]) ** 2) + ((B - color[2]) ** 2)
+				mask = euclidiano < (13 ** 2)
 			# Calcula a diferenca de intensidade para imagens em tons de cinza
 			else:
 				diferenca = abs(R - color[0])
 				mask = diferenca < 13
+			# Colore os pixels
 			raw[np.where(mask)] = [0,0,255]
 			color = None
 		if cv2.waitKey(25) & 0xFF == ord('q'):
 			cv2.destroyAllWindows()
 			break
+
+elif flag == 3:
+	print "╔═══════════════════════════════════════╗"
+	print "║	Escolha o arquivo de entrada	║"
+	print "╠════════════════════╦══════════════════╣"
+	print "║ Vídeo Colorido     ║ digite 1		║"
+	print "║ Escolher Arquivo   ║ nome do Arquivo 	║"
+	print "║	Sair	     ║ digite 0		║"
+	file = raw_input("╚════════════════════╩══════════════════╝\n")
+	if file == '0':
+		cv2.destroyAllWindows()
+		flag = 0
+	elif file == '1':
+		cap = cv2.VideoCapture('../data/test.avi')
+	elif file == '2':
+		cap = cv2.VideoCapture('../data/testPeB.avi')
+	else:
+		cap = cv2.VideoCapture(file)
+
+
+elif flag == 4:
+	cap = cv2.VideoCapture(0)
+
+if flag == 3 or flag == 4:
+	print "╔═══════════════════════════════╗"
+	print "║	Para sair pressione 'q'	║"
+	print "╚═══════════════════════════════╝\n"
+
+	cv2.namedWindow("Trabalho")
+	cv2.setMouseCallback("Trabalho", Cor_e_Posicao)
+	# Verifica se foi possivel abrir o arquivo
+	if (cap.isOpened()== False): 
+	 	print("Erro abrindo o arquivo")
+	 
+	# Le o video
+	while(cap.isOpened()):
+		ret, image = cap.read()
+		if ret == True:
+			raw = image.copy()
+			if color is not None:
+				# Separa as 3 cores
+				R = raw[:, :, 0].astype(np.float32)
+				G = raw[:, :, 1].astype(np.float32)
+				B = raw[:, :, 2].astype(np.float32)
+				raw = image.copy()
+				# Calcula quais pixels serão marcados com base na distância euclidiana para imagens coloridas
+				if color[0] == color[1] == color[2]:
+					euclidiano = ((R - color[0]) ** 2) + ((G - color[1]) ** 2) + ((B - color[2]) ** 2)
+					mask = euclidiano < (13 ** 2)
+				# Calcula a diferenca de intensidade para imagens em tons de cinza
+				else:
+					diferenca = abs(R - color[0])
+					mask = diferenca < 13
+				# Colore os pixels
+				raw[np.where(mask)] = [0,0,255]
+			cv2.imshow('Trabalho',raw)
+			if cv2.waitKey(25) & 0xFF == ord('q'):
+				cv2.destroyAllWindows()
+				break
+
+		else: 
+			break
+	cap.release()
 
 cv2.destroyAllWindows()
 
